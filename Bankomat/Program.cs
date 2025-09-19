9d3edcf7fc08e50668f2866df82c6d2cba318dbb
Jenny Khranovska <jenny.khranovska.syne25lin@edu.tucsweden.se>
2025-09-19 14:12:17 +0200

    Skapade variabler, skapad en class som ska innehålla kund info. Logga in metod påbörjad

--- Bankomat/Program.cs
+++ Bankomat/Program.cs
@@ -3,8 +3,35 @@
     internal class Program
     {
         static void Main(string[] args)
+
         {
-            Console.WriteLine("Hello, World!");
+
+            int constant = 3;
+            bool inLoggad = false;
+
+            var Custumer1 = new Custumer();
+
+            LoggaIN(Custumer1);
+            
+            //inLoggad = LoggaIN();
+
         }
+
+        public static bool LoggaIN(Custumer custumer1)
+        {
+            int valdPin;
+            Console.Write("Ange pinkod ");
+            Console.ReadLine();
+            
+          
+
+           
+            
+            
+            return false;
+        }
+
     }
+
+
 }
---
+++ Bankomat/custumer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bankomat
+{
+    internal class Custumer
+    {
+        private int pinKod;
+
+        public int PinKod 
+        { 
+            get => pinKod;
+        
+        }
+
+        public Custumer()
+        {
+            pinKod = 1234;
+        }
+
+
+    }
+
+
+}
